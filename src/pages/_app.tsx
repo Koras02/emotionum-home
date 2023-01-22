10f5ac6c80e53b6b8842e6f0bdb291c24a99e0ef
Koras02 <wjdgh0727@gmail.com>
2023-01-22 23:23:29 +0900

    finish

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from "next/app"
-import '../styles/index.css'
-import "tailwindcss/tailwind.css"
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  future: {
+    hoverOnlyWhenSupported: true,
   },
-  variants: {
-    extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        highlight: "inset 0 0 0 1px rgba(255, 255, 255, 0.05)",
+      },
+      screens: {
+        narrow: { raw: "(max-aspect-ratio: 3 / 2)" },
+        wide: { raw: "(min-aspect-ratio: 3 / 2)" },
+        "taller-than-854": { raw: "(min-height: 854px)" },
+      },
+    },
   },
   plugins: [],
 };
